2b2afb524c23ac4deea690ae18cc3fd50de299ec
datright <84527629+datright@users.noreply.github.com>
2021-05-25 20:42:55 -0400

    Update game.py

--- game.py
+++ game.py
@@ -6,3 +6,18 @@ user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 
 #print(user_choice)
 print("User Choice: ", user_choice)
+
+
+
+# valudate the input such that only if it is one of the expected values
+# ... will we continute with the rest of the program
+# ... otherwise we'll stop the program before it tries to do anything else
+# ... and we'll ask the user to run the program again
+
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print ("Valid. Keep going")
+else:
+    print("OPPS. Invalid input. Try it again.")
+    exit()
+
+print("This is the end of our game. Play again!")
